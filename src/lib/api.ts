787b0b0ecdb03f35b9b0d3d8406378ac3599b04c
Prateek Kashyap <kashyapprateek13@gmail.com>
2024-11-15 16:20:07 +0530

    fix: added axios

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,16 +1,33 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 
-export const fetchTestNames = async () => {
+interface ApiResponse<T> {
+  status: string;
+  status_code: number;
+  data: T;
+}
+
+const BASE_URL = "http://0.0.0.0:1337";
+
+export const fetchGetApi = async <T>(endpoint: string): Promise<T> => {
   try {
-    const response = await axios.get("http://0.0.0.0:1337/test/tests/");
-    if (response.data.status === "success" && response.data.status_code === 200) {
+    const response: AxiosResponse<ApiResponse<T>> = await axios.get(
+      `${BASE_URL}${endpoint}`
+    );
+    if (
+      response.data.status === "success" &&
+      response.data.status_code === 200
+    ) {
       return response.data.data;
     } else {
       console.error("Unexpected API response:", response);
-      return [];
+      return [] as unknown as T;
     }
   } catch (error) {
-    console.error("Error fetching test names:", error);
-    return [];
+    console.error(`Error fetching data from ${endpoint}:`, error);
+    return [] as unknown as T;
   }
+};
+
+export const fetchTestNames = async (): Promise<string[]> => {
+  return await fetchGetApi<string[]>("/test/tests/");
 };
